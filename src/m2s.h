427d63d36309a24ee66d10485f8e9ab6506dd39a
stardica <stardica@gmail.com>
2015-02-18 15:32:28 -0500

    na

--- src/m2s.h
+++ src/m2s.h
@@ -12,10 +12,10 @@
 
 //GPU 1 or 0 takes in and out all of the GPU and runtime code
 //CGM 1 or 0 takes in and out the new memory system
-#define GPU 0
-#define CGM 1
+#define GPU 1
+#define CGM 0
 
 //MSG turns on and off verbose notes
 // MSG 1 outputs configuration data.
 // MSG 2 outputs memory system runtime data.
-#define MSG 1
+#define MSG 0
