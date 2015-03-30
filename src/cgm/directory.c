794f97c4d582d65e3e3d7261d3780257b9f1e507
stardica <stardica@gmail.com>
2015-03-30 14:02:22 -0400

    Added directory

---
+++ src/cgm/directory.c
@@ -0,0 +1,8 @@
+/*
+ * directory.c
+ *
+ *  Created on: Mar 30, 2015
+ *      Author: stardica
+ */
+
+
---
+++ src/cgm/directory.h
@@ -0,0 +1,22 @@
+/*
+ * directory.h
+ *
+ *  Created on: Mar 30, 2015
+ *      Author: stardica
+ */
+
+
+#ifndef DIRECTORY_H_
+#define DIRECTORY_H_
+
+
+
+
+
+
+
+
+
+
+
+#endif /*DIRECTORY_H_*/
