4de809318536e076bb8cd1e9e5cc2f9cca7c372f
stardica <stardica@stardica-hec-u14-64bit>
2016-10-13 16:51:57 -0400

    update

--- src/cgm/cgm.h
+++ src/cgm/cgm.h
@@ -55,8 +55,8 @@
 #define AWAIT_P_PHI0 if (etime.count & 0x1) epause(1)
 #define AWAIT_P_PHI1 if (!(etime.count & 0x1)) epause(1)
 #define PRINT(message, ...)	printf(message, __VA_ARGS__); fflush(stdout)
-#define WATCHBLOCK (unsigned int) 0x0004a540
-#define WATCHLINE 0
+#define WATCHBLOCK (unsigned int) 0x0333e1c0
+#define WATCHLINE 1
 //Level 0 = no blk trace, 1 = L1-L2, 2 = L2-L3, 3 = L1-L3,
 #define LEVEL 2
 //dumps the system trace 0 off 1 on
